32319883178eb17647cd7ec31c6480a10752ca88
theonlytruealex <alexiova264@gmail.com>
2026-08-24 10:11:27 +0300

    added point doublinig and point addition

--- kernel/src/hil/crypto/elliptic_curves/ecc_math.rs
+++ kernel/src/hil/crypto/elliptic_curves/ecc_math.rs
@@ -10,17 +10,20 @@ use crate::hil::crypto::elliptic_curves::ecc_constants::Curve;
 /// Upcall from the `EccCryptoBase` trait.
 pub trait Client<'a> {
     fn read_scalar(&self, scalar: &mut [u8]) -> Result<(), ErrorCode>;
-    fn read_base_point(&self, base_point: &mut [u8]) -> Result<(), ErrorCode>;
+    fn read_point(&self, point: &mut [u8]) -> Result<(), ErrorCode>;
+    fn read_second_point(&self, point: &mut [u8]) -> Result<(), ErrorCode>;
     fn write_point(&self, point: &[u8]) -> Result<(), ErrorCode>;
     fn multiplication_done(&self, result: Result<(), ErrorCode>);
 }
 
 pub trait EccCryptoBase<'a> {
-    /// Set the `Client` client to be called on completion.
     fn set_client(&'a self, client: &'a dyn Client<'a>);
 
-    /// Clear any confidential data.
     fn clear_data(&self);
+
+    fn point_doubling<const P_SIZE: usize, C: Curve<P_SIZE>>(&self) -> Result<(), ErrorCode>;
+
+    fn point_addition<const P_SIZE: usize, C: Curve<P_SIZE>>(&self) -> Result<(), ErrorCode>;
 
     fn scalar_multiplicaiton<const P_SIZE: usize, C: Curve<P_SIZE>>(
         &self,
